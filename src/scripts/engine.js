356c6cc3016340fec1234bf05c8d8bcb31e17da7
KaioRapanos <kaiorapanos@gmail.com>
2023-09-27 22:19:45 -0300

    Creating game styling

---
+++ src/scripts/engine.js
@@ -0,0 +1,31 @@
+const emojis = [
+    "🕷",
+    "🕷",
+    "🦋",
+    "🦋",
+    "🦇",
+    "🦇",
+    "🦉",
+    "🦉",
+    "🦅",
+    "🦅",
+    "🐠",
+    "🐠",
+    "🦈",
+    "🦈",
+    "🐎",
+    "🐎",
+];
+
+let openCards = [];
+
+let shuffleEmojis = emojis.sort(()=> {
+    (Math.random() < 0.5 ? 2 : -1)
+});
+
+for (let i = 0; i < emojis.length; i++) {
+    let box = document.createElement("div");
+    box.className = "item";
+    box.innerHTML = shuffleEmojis[i];
+    document.querySelector(".game").appendChild(box)
+}
